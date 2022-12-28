46483b4cca6ec3ca5b8bef46e059622f64f6b015
RCP161 <30856169+RCP161@users.noreply.github.com>
2022-12-28 11:28:48 +0100

    test

---
+++ CoreWCF.Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using CoreWCF;
+using CoreWCF.Configuration;
+using CoreWCF.Description;
+using CoreWCF.Server;
+
+var builder = NetApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.AllowSynchronousIO = true;
+});
+
+// Add WSDL support
+builder.Services.AddServiceModelServices().AddServiceModelMetadata();
+builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+
+var app = builder.Build();
+app.UseServiceModel(builder =>
+{
+    builder.AddService((serviceOptions) => { })
+    // Add a BasicHttpBinding at a specific endpoint
+    .AddServiceEndpoint<EchoService, IEchoService>(new NetTcpBinding(), "/EchoService")
+});
+var serviceMetadataBehavior = app.Services.GetRequiredService();
+serviceMetadataBehavior.HttpGetEnabled = true;
+
+app.Run();
