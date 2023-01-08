77b6c7665b2d29be58a2bf2eb0a15aa5b0b33fc5
RCP161 <30856169+RCP161@users.noreply.github.com>
2023-01-08 15:37:48 +0100

    asp.net core test

---
+++ CoreWCFService1/ServerHost.cs
@@ -0,0 +1,24 @@
+﻿namespace CoreWCFService1
+{
+    public class ServerHost
+    {
+        public ServerHost() { }
+
+        public void Start()
+        {
+            var builder = WebApplication.CreateBuilder();
+
+            builder.Services.AddServiceModelServices();
+
+            var app = builder.Build();
+
+            app.UseServiceModel(serviceBuilder =>
+            {
+                serviceBuilder.AddService<Service>();
+                serviceBuilder.AddServiceEndpoint<Service, IService>(new NetHttpBinding(), "/Service.svc");
+            });
+
+            app.Run();
+        }
+    }
+}
---
+++ Prototyp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototyp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the WCF replacement prototype");
+            Console.WriteLine("========================================");
+            Console.WriteLine();
+            Console.WriteLine("Please enter the port:");
+
+
+            Console.ReadLine();
+
+            ServerHost host = new ServerHost();
+
+        }
+    }
+}
